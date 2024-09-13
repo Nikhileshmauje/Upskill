2716bc9464e5bf11703f8af7cb5fa64eaf0160e1
Aman Varma <vaman0118@gmail.com>
2024-09-13 13:35:57 +0530

    update Home Page

--- Pages/profile.js
+++ Pages/profile.js
@@ -24,3 +24,31 @@ const chartData = {
       },
     },
   });
+
+const notificationBtn = document.getElementById('notification-btn');
+          const notificationPopup = document.getElementById('notification-popup');
+
+          notificationBtn.addEventListener('mouseover', () => {
+            notificationPopup.style.display = 'block';
+          });
+
+          notificationBtn.addEventListener('click', () => {
+            if (notificationPopup.style.display === 'block') {
+              notificationPopup.style.display = 'none';
+            } else {
+              notificationPopup.style.display = 'block';
+            }
+          });
+
+          notificationPopup.addEventListener('mouseover', () => {
+            notificationPopup.style.display = 'block';
+          });
+
+          function checkIfShouldClose(event) {
+            if (!notificationPopup.contains(event.relatedTarget) && !notificationBtn.contains(event.relatedTarget)) {
+              notificationPopup.style.display = 'none';
+            }
+          }
+
+          notificationBtn.addEventListener('mouseout', checkIfShouldClose);
+          notificationPopup.addEventListener('mouseout', checkIfShouldClose);
